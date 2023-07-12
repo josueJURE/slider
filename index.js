54d181bda375f45f76082a0187e481ffc4750c62
josueJURE <josue.jure@gmail.com>
2023-07-12 16:01:20 +0100

    feat: add eventListener to chevRight element

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-
 (function () {
   const slides = document.querySelectorAll(".slide");
   const slide = document.querySelector(".slide-group");
   const dotSection = document.querySelector(".dotSection");
+  const chevLeft = document.querySelector(".fa-chevron-left");
+  const chevRight = document.querySelector(".fa-chevron-right");
+  console.log({ chevRight }, { slide });
+
   let currentSlide = 0;
+  let clear;
 
   // add dots.
   slides.forEach((_) => {
@@ -12,27 +16,52 @@
     dotSection.appendChild(element);
   });
 
-  const advance = (index) => {
+  function advance(index) {
     const element = slides[index];
     element.style.display = "none";
-    dotSection.childNodes[index].classList.remove('addRed');
+    dotSection.childNodes[index].classList.remove("addRed");
     const isGreater = index + 1 >= slides.length;
-    if(isGreater) {
+    if (isGreater) {
       slides[0].style.display = "block";
-      dotSection.childNodes[0].classList.add('addRed');
+      dotSection.childNodes[0].classList.add("addRed");
     } else {
       slides[index + 1].style.display = "block";
-      dotSection.childNodes[index + 1].classList.add('addRed');
-
+      dotSection.childNodes[index + 1].classList.add("addRed");
     }
-    setTimeout(() => {
-      if(isGreater) {
+    clear = setTimeout(() => {
+      if (isGreater) {
         advance(0);
       } else {
         advance(index + 1);
       }
     }, 3000);
-  };
+  }
 
   advance(currentSlide);
+
+  chevRight.addEventListener("click", function () {
+    clearTimeout(clear);
+    slides[currentSlide].style.display = "none";
+    dotSection.childNodes[currentSlide].classList.remove("addRed");
+    if (currentSlide < slides.length - 1) {
+      currentSlide++;
+      slides[currentSlide].style.display = "block";
+      dotSection.childNodes[currentSlide].classList.add("addRed");
+     
+    } else {
+      currentSlide = 0;
+      slides[currentSlide].style.display = "block";
+    dotSection.childNodes[currentSlide].classList.add("addRed");
+    }
+  });
+
+  // chevRight.addEventListener("click", function () {
+  //   if (currentSlide < slides.length - 1) {
+  //     currentSlide++;
+  //   } else {
+  //     currentSlide = 0;
+  //   }
+  //   console.log("josué")
+  //   slides[currentSlide].style.display = "block";
+  // });
 })();
