bb21417ef7f7c2dd5314fe0b288cb628af8ad7a1
josueJURE <josue.jure@gmail.com>
2023-07-12 10:39:58 +0100

    refactor: allow images and button to change at the same time

--- index.js
+++ index.js
@@ -1,65 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () {
+
+(function () {
   const slides = document.querySelectorAll(".slide");
   const slide = document.querySelector(".slide-group");
   const dotSection = document.querySelector(".dotSection");
-
-  console.log(dotSection)
-
   let currentSlide = 0;
-  let theInterval 
 
-  function advance() {
-    theInterval = setTimeout(function() {
-      slides[currentSlide].style.display = "none";
-      if(currentSlide < slides.length - 1) {
-        x[currentSlide].classList.add('addRed')
-        currentSlide++;
-        console.log(x[currentSlide])
-      } else {
-        currentSlide = 0;
-        x[currentSlide].classList.remove('addRed')
-
-      }
-      slides[currentSlide].style.display = "block";
-     
-      advance();
-     
-     
-
-    }, 3000)
-    x[currentSlide].classList.remove('addRed')
-
-  }
-
-  // function stopSlides() {
-  //   clearInterval(theInterval);
-  //   console.log("josue")
-  // }
-
-  // slide.addEventListener("mouseover", stopSlides)
-
+  // add dots.
   slides.forEach((_) => {
     let element = document.createElement("div");
     element.innerHTML = `<i class="fas fa-dot-circle"></i>`;
     dotSection.appendChild(element);
+  });
 
-   
+  const advance = (index) => {
+    const element = slides[index];
+    element.style.display = "none";
+    dotSection.childNodes[index].classList.remove('addRed');
+    const isGreater = index + 1 >= slides.length;
+    if(isGreater) {
+      slides[0].style.display = "block";
+      dotSection.childNodes[0].classList.add('addRed');
+    } else {
+      slides[index + 1].style.display = "block";
+      dotSection.childNodes[index + 1].classList.add('addRed');
 
-   
+    }
+    setTimeout(() => {
+      if(isGreater) {
+        advance(0);
+      } else {
+        advance(index + 1);
+      }
+    }, 3000);
+  };
 
-  })
-
-  let x = document.querySelectorAll(".fa-dot-circle");
-  console.log({nodeList: x, index: currentSlide})
- 
-
-
-
-
-  
-  advance()
-
-  
-});
-
-
+  advance(currentSlide);
+})();
